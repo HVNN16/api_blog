c6220c9cbc79373a7ad80a8f460c75b1e4d63bc5
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2024-11-15 10:41:21 +0700

    change usercontroller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userController.js');
 
 // Hiển thị danh sách người dùng
 router.get('/manage', userController.manageUsers);
